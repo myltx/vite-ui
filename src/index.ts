1c8f03a46d2487c4eb3ba616453fca89728218bf
mayunlongtx <865147643@qq.com>
2022-08-20 23:58:22 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import SButton from "./button/index";
+import SFCButton from "./SFCButton.vue";
+import JSXButton from "./JSXButton";
+
+createApp(JSXButton).mount("#app");
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
+
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 添加JSX插件
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+  ],
+  // 添加库模式配置
+  build: {
+    rollupOptions,
+    minify: false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["es", "umd", "iife"],
+    },
+  },
+});
